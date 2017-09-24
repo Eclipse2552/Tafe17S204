d269352ab288a99f2c79ec4b0f0296570df2e673
Yash Shelatkar <shelatkaryash@gmail.com>
2017-09-24 02:31:43 -0700

    Appointments.cs Class File added & Hamburger Button Done

---
+++ Start-Finance-master/InstaRichie/Models/Appointments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+
+namespace StartFinance.Models
+{
+    class Appointments
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+
+        [Unique]
+
+        public String appointmentName { get; set; }
+
+        [NotNull]
+        public DateTime appointmentDate { get; set; }
+
+        [NotNull]
+        public DateTime StartTime { get; set; }
+
+        [NotNull]
+        public DateTime EndTime { get; set; }
+    }
+}
