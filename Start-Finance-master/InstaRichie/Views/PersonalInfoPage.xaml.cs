afcd882eaabd65447671dd3b0bd70b51e0230cda
Aden Kleemann <eclipse2552@hotmail.com.au>
2017-09-24 22:06:35 +0930

    Add most background code and switched UI to the apps style. Have bugs such as not being able to interact with the textboxes though.

--- Start-Finance-master/InstaRichie/Views/PersonalInfoPage.xaml.cs
+++ Start-Finance-master/InstaRichie/Views/PersonalInfoPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using SQLite.Net;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using StartFinance.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +25,108 @@ namespace StartFinance.Views
     /// </summary>
     public sealed partial class PersonalInfoPage : Page
     {
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+
         public PersonalInfoPage()
         {
             this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            // Creating table
+            Results();
+        }
+
+        public void Results()
+        {
+            conn.CreateTable<PersonalInfo>();
+            var query1 = conn.Table<PersonalInfo>();
+            PersonalInfoView.ItemsSource = query1.ToList();
+        }
+
+        private async void AddWish_Click(object sender, RoutedEventArgs e)
+        {
+            /*try
+            {
+                if (tbFirstName.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No value entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    double TempMoney = Convert.ToDouble(tbFirstName.Text);
+                    conn.CreateTable<PersonalInfo>();
+                    conn.Insert(new PersonalInfo
+                    {
+                        WishName = tbFirstName.Text.ToString(),
+                        Money = TempMoney
+                    });
+                    // Creating table
+                    Results();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    MessageDialog dialog = new MessageDialog("You forgot to enter the Amount or entered an invalid Amount", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (ex is SQLiteException)
+                {
+                    MessageDialog dialog = new MessageDialog("Wish Name already exist, Try Different Name", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    /// no idea
+                }
+            }*/
+
+            conn.CreateTable<PersonalInfo>();
+            conn.Insert(new PersonalInfo
+            {
+                FirstName = tbFirstName.Text,
+                LastName = tbLastName.Text,
+                DOB = tbDoB.Text,
+                Gender = tbGender.Text,
+                Email = tbEmail.Text,
+                Phone = tbPhone.Text
+            });
+            // Creating table
+            Results();
+        }
+
+        /*private async void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string AccSelection = ((PersonalInfo)PersonalInfoView.SelectedItem).WishName;
+                if (AccSelection == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    conn.CreateTable<PersonalInfo>();
+                    var query1 = conn.Table<PersonalInfo>();
+                    var query3 = conn.Query<PersonalInfo>("DELETE FROM WishList WHERE WishName ='" + AccSelection + "'");
+                    PersonalInfoView.ItemsSource = query1.ToList();
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                await dialog.ShowAsync();
+            }
+        }*/
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
         }
     }
 }
